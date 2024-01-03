8658be4dc43927b2f12b5d1d05241eb809981baa
dwj <daiwenjuan668@163.com>
2024-01-03 11:07:32 +0800

    gitignore

---
+++ pink-rabbit/rabbit-scripts/dist/main.fdff17d85ef83473a882.bundle.js
@@ -0,0 +1,53 @@
+"use strict";
+(self.webpackChunkrabbit_scripts = self.webpackChunkrabbit_scripts || []).push([
+  [179],
+  {
+    907: (e, t, s) => {
+      var n = s(294),
+        l = s(745),
+        r = s(379),
+        o = s.n(r),
+        a = s(795),
+        c = s.n(a),
+        i = s(569),
+        b = s.n(i),
+        u = s(565),
+        d = s.n(u),
+        m = s(216),
+        p = s.n(m),
+        _ = s(589),
+        f = s.n(_),
+        h = s(736),
+        k = {};
+      (k.styleTagTransform = f()),
+        (k.setAttributes = d()),
+        (k.insert = b().bind(null, "head")),
+        (k.domAPI = c()),
+        (k.insertStyleElement = p()),
+        o()(h.Z, k),
+        h.Z && h.Z.locals && h.Z.locals,
+        l
+          .createRoot(document.getElementById("root"))
+          .render(
+            n.createElement("div", { className: "test" }, "Hello, world!1111")
+          );
+    },
+    736: (e, t, s) => {
+      s.d(t, { Z: () => a });
+      var n = s(81),
+        l = s.n(n),
+        r = s(645),
+        o = s.n(r)()(l());
+      o.push([
+        e.id,
+        ".Qf8_RmMLxRyw6oi1bs_u{\n    background-color:blueviolet;\n}",
+        "",
+      ]),
+        (o.locals = { test: "Qf8_RmMLxRyw6oi1bs_u" });
+      const a = o;
+    },
+  },
+  (e) => {
+    e.O(0, [216], () => (907, e((e.s = 907)))), e.O();
+  },
+]);
